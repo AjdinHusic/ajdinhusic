447043bcb675167a90d6fbc4c5e4056f01be491e
Ajdin <ajdin@procc.nl>
2026-03-26 20:22:30 +0100

    Add Privacy Policy page, update index and nginx configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,16 @@ import {defineConfig} from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  appType: "mpa",
   plugins: [
     tailwindcss()
-  ]
+  ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        privacy: "privacy.html"
+      }
+    }
+  }
 })
